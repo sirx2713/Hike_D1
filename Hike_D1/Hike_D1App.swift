5ece63048f158d907336bd614f4caea1e2cc7c40
sirx2713 <bradx2713@gmail.com>
2024-03-29 07:52:01 +0200

    Initial Commit

---
+++ Hike_D1/Hike_D1App.swift
@@ -0,0 +1,17 @@
+//
+//  Hike_D1App.swift
+//  Hike_D1
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/03/29.
+//
+
+import SwiftUI
+
+@main
+struct Hike_D1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
